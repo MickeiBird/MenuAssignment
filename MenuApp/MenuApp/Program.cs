355df003deadce8fea0d2ff230f0b7375947d378
MickeiBird <madmofoforeal@gmail.com>
2017-08-17 11:00:50 +0200

    hope this works

--- MenuApp/MenuApp/Program.cs
+++ MenuApp/MenuApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MenuApp
 {
@@ -6,7 +7,101 @@ namespace MenuApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] menuItems = {
+                "List of all videos",
+                "Add video",
+                "Delete video",
+                "Edit video",
+                "Exit"
+            };
+
+            List<Video> videoList = new List<Video>(); 
+            var selection = Menu(menuItems);
+
+            while (selection != 5)
+            {
+                switch (selection)
+                {
+                    case 1:
+                        Console.WriteLine("\nShowing list of all videos...\n");
+                        listVideos(videoList);
+                        break;
+                    case 2:
+                        Console.WriteLine("\nAdd videos...\n");
+                        Console.WriteLine("Please type the Name of the video\n");
+                        string name = Console.ReadLine(); 
+                        addVideo(name, videoList);
+                        
+                        break;
+                    case 3:
+                        Console.WriteLine("Delete video...");
+                        break;
+                    case 4:
+                        Console.WriteLine("Edit video...");
+                        break;
+                    case 5:
+                        Console.WriteLine("Exiting...");
+                        break;
+                }
+                Console.WriteLine("Press enter to select another option");
+                Console.ReadLine();
+                
+                selection = Menu(menuItems);
+            }
+
+
+            Console.ReadLine();
         }
+
+
+        private static int Menu(string[] menuItems)
+        {
+            Console.Clear();
+            Console.WriteLine("Select what you want to do: \n");
+            Console.WriteLine("---------------------------\n");
+
+            for (int i = 0; i < menuItems.Length; i++)
+            {
+                Console.WriteLine($"{i + 1} : {menuItems[i]} \n");
+            }
+
+            int selection;
+            while (!int.TryParse(Console.ReadLine(), out selection) || selection < 1 || selection > 5)
+            {
+                Console.WriteLine("You need to select a number from 1-5");
+            }
+            Console.WriteLine("\nSelection : " + selection);
+            return selection; 
+        }
+
+        private static List<Video> addVideo(string name, List<Video> videoList)
+        {
+            
+            Video video = new Video();
+            video.name = name;
+            for (int i = 0; i < videoList.Count; i++)
+            {
+                if (videoList[i].id.Equals(null))
+                {
+                    videoList[i].id = i;
+                    Console.WriteLine("{0} is the id of the video", i);
+                }
+            }
+
+            videoList.Add(video);
+            Console.WriteLine("\n{0} was added to videos : \n ", name);
+            
+            return videoList;
+        }
+        
+        private static void listVideos(List<Video> vNames)
+        {
+            foreach (var vName in vNames)
+            {
+                Console.WriteLine(vName.name + "\n");
+            }
+        }
+
+        
     }
 }
---
+++ MenuApp/MenuApp/Video.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MenuApp
+{
+    class Video
+    {
+        public int id { get; set; }
+        //public int genre { get; set; }
+        public string name { get; set; }
+        //public int MyProperty { get; set; }
+
+
+    }
+}
